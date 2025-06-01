04f521157cf514db978256a1e2855a4f60233d99
KaueFernandes01 <140000749+KaueFernandes01@users.noreply.github.com>
2025-06-01 04:01:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
