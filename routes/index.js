8304b9959c337c499fcd6498a2808f240e1b6e74
VisuallyImpairedMap <visuallyimpairedmap@gmail.com>
2025-06-08 22:01:44 -0300

    beta1.3

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const usuarioRoutes = require('./usuarios');
 const loginRoute = require('./login');
 const motorRoutes = require('./motor');
 const autenticarToken = require('../src/middlewares/autenticarUsuario');
-const logRoutes = require('./logs');
+const logRoutes = require('./log');
 
 router.use('/login', loginRoute);
 router.use('/usuarios', usuarioRoutes);
 router.use('/motor', autenticarToken, motorRoutes)
-router.use('/logs', autenticarToken, logRoutes);
+router.use('/log', autenticarToken, logRoutes);
 
 module.exports = router;
